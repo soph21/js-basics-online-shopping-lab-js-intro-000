a6b37b86317388012b954ebfa39f8fd114a58f6f
Sophie Shin <sophie@alloy.co>
2021-03-19 14:15:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ for (let b = 0; b < cart.length; b++) {
     itemExists = 1
   }
 }
-  if (itemExists = 0) {
+  if (itemExists === 0) {
     return "That item is not in your cart."
 } else {
   return cart
