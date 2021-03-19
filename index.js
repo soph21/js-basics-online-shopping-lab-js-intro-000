e063447f9cd527be5dc051c3778267900719c496
Sophie Shin <sophie@alloy.co>
2021-03-19 16:29:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,9 +60,8 @@ function placeOrder(cardNumber) {
     if (cardNumber) {
       let totalCost = total()
       cart.splice(0, cart.length)
-      cardReceived = 1
       return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
-    } else { 
+    } else {
       return "Sorry, we don't have a credit card on file for you."
   }
 }
