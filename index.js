0f8768ee1047193bbec842b609300ef8d5e132de
Sophie Shin <sophie@alloy.co>
2021-03-19 16:18:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ function placeOrder(cardNumber) {
     }
   }
     if (cardReceived ===0) {
-      return "Sorry, we don't have a credit card on file for you".
+      return "Sorry, we don't have a credit card on file for you."
   } else {
     return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
   }
